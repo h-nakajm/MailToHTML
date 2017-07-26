033e50f7073190930bf1286e270a7ad036d7966c
U-H-NAKAJM\hiroki <h-nakajm@ist.osaka-u.ac.jp>
2017-07-26 10:52:23 +0900

    modified:   MailToHTML.rb 	read lib.html

--- MailToHTML.rb
+++ MailToHTML.rb
@@ -3,12 +3,22 @@ out_file = 'out.html'
 
 # 入力ファイルを配列に読み込む
 input = []
-f = File.open(ARGV[0], mode = "r") { |f|
+input_file = File.open(ARGV[0], mode = "r") { |f|
   input = f.readlines
+}
+
+# 雛形を配列に読み込む
+lib = []
+lib_file = File.open('lib.html', mode = "r") { |f|
+  lib = f.readlines
 }
 
 # 最後のSubjectを検索
 subject_index = input.rindex { |item|
   item =~ /^Subject/
 }
+
 puts subject_index
+
+# 雛形への挿入箇所を検索
+# <body>の次の行
