d3c6bf2b3579a76edfd497b6ca83f0d0a370fead
U-H-NAKAJM\hiroki <h-nakajm@ist.osaka-u.ac.jp>
2017-07-26 13:27:05 +0900

    modified:   MailToHTML.rb 	add output with body tag.

--- MailToHTML.rb
+++ MailToHTML.rb
@@ -1,6 +1,9 @@
 # 出力ファイル名を設定
 out_file = 'out.html'
 
+# ファイル出力用配列
+output = []
+
 # 入力ファイルを配列に読み込む
 input = []
 input_file = File.open(ARGV[0], mode = "r") { |f|
@@ -13,11 +16,16 @@ lib_file = File.open('lib.html', mode = "r") { |f|
   lib = f.readlines
 }
 
-# 最後のSubjectを検索
+# メール内の最後のSubjectを検索
 subject_index = input.rindex { |item|
   item =~ /^Subject/
 }
 
 # 雛形への挿入箇所を検索
 # <body>の次の行
-body_index = lib.index { |item| item =~ /^<body>/ }
+body_index = lib.index { |item|
+  item =~ /^<body>/
+}
+
+# libの<body>までを配列outputに入れる
+output << lib[0, body_index + 1]
