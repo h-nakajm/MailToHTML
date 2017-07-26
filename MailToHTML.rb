17f968d9b5a6712fd77fb20babee681948eeec6b
U-H-NAKAJM\hiroki <h-nakajm@ist.osaka-u.ac.jp>
2017-07-26 18:00:59 +0900

    modified:   MailToHTML.rb 	escape HTML meta characters.

--- MailToHTML.rb
+++ MailToHTML.rb
@@ -5,12 +5,17 @@ out_file = 'out.html'
 input = []
 File.open(ARGV[0], mode = "r") { |f| input = f.readlines }
 
+# HTMLの特殊文字をサニタイズ
+require 'cgi'
+escaped_input = []
+input.each { |item| escaped_input << CGI.escapeHTML(item)}
+
 # 雛形を配列に読み込む
 lib = []
 File.open('lib.html', mode = "r") { |f| lib = f.readlines }
 
 # メール内の最後のSubjectを検索
-subject_index = input.rindex { |item| item =~ /^Subject/ }
+subject_index = escaped_input.rindex { |item| item =~ /^Subject/ }
 
 # 雛形への挿入箇所を検索
 # <body>の次の行
@@ -21,7 +26,7 @@ output = []
 output << lib[0, body_index + 1]
 
 # 最後の「Subject」以降の文面の各行の最後に「</br>」を追加してoutputに追記
-output << input[subject_index + 1, input.length - 1].map { |item| item + '</br>' }
+output << escaped_input[subject_index + 1, input.length - 1].map { |item| item + '</br>' }
 
 # outputに雛形の残りを追記
 output << lib[body_index + 1, lib.length - 1]
