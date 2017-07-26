0a8f5e0c3b572529cec21aabf5a4fb320451af68
U-H-NAKAJM\hiroki <h-nakajm@ist.osaka-u.ac.jp>
2017-07-26 14:01:45 +0900

    modified:   MailToHTML.rb 	output to out.html finished.

--- MailToHTML.rb
+++ MailToHTML.rb
@@ -1,16 +1,13 @@
 # 出力ファイル名を設定
 out_file = 'out.html'
 
-# ファイル出力用配列
-output = []
-
 # 入力ファイルを配列に読み込む
 input = []
-input_file = File.open(ARGV[0], mode = "r") { |f| input = f.readlines }
+File.open(ARGV[0], mode = "r") { |f| input = f.readlines }
 
 # 雛形を配列に読み込む
 lib = []
-lib_file = File.open('lib.html', mode = "r") { |f| lib = f.readlines }
+File.open('lib.html', mode = "r") { |f| lib = f.readlines }
 
 # メール内の最後のSubjectを検索
 subject_index = input.rindex { |item| item =~ /^Subject/ }
@@ -20,10 +17,14 @@ subject_index = input.rindex { |item| item =~ /^Subject/ }
 body_index = lib.index { |item| item =~ /^<body>/ }
 
 # libの<body>までを配列outputに入れる
+output = []
 output << lib[0, body_index + 1]
 
 # 最後の「Subject」以降の文面の各行の最後に「</br>」を追加してoutputに追記
-output << input[subject_index, input.length - 1].map { |item| item + '</br>' }
+output << input[subject_index + 1, input.length - 1].map { |item| item + '</br>' }
 
 # outputに雛形の残りを追記
 output << lib[body_index + 1, lib.length - 1]
+
+# outputをファイルに出力
+File.open(out_file, mode = "w") { |f| output.each { |s| f.puts(s) } }
