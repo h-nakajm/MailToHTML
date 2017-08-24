e34ac56e2a54020d74c98c58ba9828c2ecdf2724
U-H-NAKAJM\hiroki <h-nakajm@ist.osaka-u.ac.jp>
2017-08-24 16:35:21 +0900

    new file:   html/lib.html 	new file:   html/questionnaire.html 	new file:   html/welcome.html 	new file:   js/lib.js 	new file:   js/questionnaire.js 	deleted:    lib.html 	deleted:    lib.js 	deleted:    questionnaire.html 	deleted:    questionnaire.js 	deleted:    welcome.html make html and js directory.

---
+++ js/lib.js
@@ -0,0 +1,155 @@
+var start;
+var dbpath = "http://127.0.0.1:8080/test/results/";
+var ad_dbpath = "http://127.0.0.1:8080/test/ad_clicked";
+var ad_open = "http://sdl.ist.osaka-u.ac.jp";
+var type = location.search.match(/type=(.*?)(&|$)/);
+
+// メール分類ボタンが押された場合
+function categorize() {
+
+  var a = "";
+
+  for (var i = 0; i < document.form1.example.length; i++) {
+
+    // ボタンが選択されている場合
+    if (document.form1.elements[i].checked) {
+
+      var end = new Date();
+      var next_page = $('#next').text() + '?id=' + $('#id').text() + '&type=' + $('#type').text();
+
+      // 選択されているボタンを取得
+      a = document.form1.elements[i].value;
+
+      // Ajaxで送信するデータを作成
+      var result = {
+        id: $('#id').text(),
+        url: document.location.href,
+        start: start,
+        end: end,
+        time: end - start,
+        type: $('#type').text(),
+        selected: a
+      }
+
+      // 作成したデータをAjaxで送信
+      $.ajax({
+        url: dbpath,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(result),
+        dataType: 'xml'
+      }).done(function(data) { // success
+        window.location.href = next_page;
+      }).fail(function(data) { // error
+        window.alert(textStatus + ": Unable to connect to the server.");
+      });
+
+      break;
+
+      // ボタンが選択されていない場合
+    } else if (i == document.form1.example.length - 1) {
+
+      window.alert('Please select the button.');
+
+    }
+
+  }
+
+}
+
+// 途中でやめるボタンが押された場合
+function quit() {
+
+  window.confirm('quit');
+
+}
+
+//ページ読み込み終了時の処理
+window.onload = function() {
+
+  start = new Date();
+
+  if (type[1] == "static") {
+    // 静止広告を挿入
+    $("#static_ad_space").after("<a href=\"http:\/\/sdl.ist.osaka-u.ac.jp\" target=\"_blank\"><img id=\"static_ad\" width=\"640\" src=\"./image/sample_ad01.png\"></a>");
+
+    // 静止広告がクリックされた時の処理
+    $("#static_ad").on('click', function() {
+      var record = {
+        id: $('#id').text(),
+        url: document.location.href,
+        date: new Date(),
+        type: "static"
+      };
+
+      // 広告がクリックされた情報をdbに送る
+      $.ajax({
+        url: ad_dbpath,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(record),
+        dataType: 'xml'
+      }).done(function(data) { // success
+        console.log("success");
+      }).fail(function(data) { // error
+        console.log("fail");
+      });
+    });
+  } else if (type[1] == "anchor") {
+    // アンカー広告を挿入
+    $("#anchor_ad_space").after("<div style=\"text-align: center;\" class=\"meerkat\"><a href=\"http://sdl.ist.osaka-u.ac.jp\" target=\"_blank\"><img border=\"0\" width=\"640\" height=\"200\" alt=\"\" src=\"./image/sample_ad01.png\"></a></div>");
+    $(function() {
+      $('.meerkat').meerkat({
+        background: 'url(\'./image/black.png\') repeat-x left top',
+        height: '200px',
+        width: '100%',
+        position: 'bottom'
+        //close: '.close-meerkat',
+        //dontShowAgain: '.dont-show',
+        //animationIn: 'slide',
+        //animationSpeed: 500,
+        //removeCookie: '.reset'
+      }).addClass('pos-bot');
+    });
+
+    // アンカー広告の両側の黒い部分が押された場合のページ遷移(新規window)
+    $('.meerkat').on({
+      'click': function() {
+        window.open(ad_open);
+        var record = {
+          id: $('#id').text(),
+          url: document.location.href,
+          date: new Date(),
+          type: "anchor"
+        };
+
+        // 広告がクリックされた情報をdbに送る
+        $.ajax({
+          url: ad_dbpath,
+          type: "POST",
+          contentType: "application/json",
+          data: JSON.stringify(record),
+          dataType: 'xml'
+        }).done(function(data) { // success
+          console.log("success");
+        }).fail(function(data) { // error
+          console.log("fail");
+        });
+      }
+    });
+  }
+
+  // bodyに動的に追加されたスタイルを削除
+  $('body').removeAttr('style');
+
+  // urlのクエリパラメタからidを取得して「your ID」に表示
+  var match = location.search.match(/id=(.*?)(&|$)/);
+  if (match) {
+    var id = match[1];
+    $('#id').text(id);
+  }
+
+  // urlのクエリパラメタからtypeを取得してhtml内に埋め込む
+  $('#type').text(type[1]);
+
+}
---
+++ js/questionnaire.js
@@ -0,0 +1,60 @@
+var dbpath = "http://127.0.0.1:8080/test/questionnaire/";
+var next_page = "http://sdl.ist.osaka-u.ac.jp";
+
+// Enterキーでの送信を許さない
+$(function() {
+  $(document).on("keypress", "input:not(.allow_submit)", function(event) {
+    return event.which !== 13;
+  });
+});
+
+function Clicked() {
+  var a = "";
+
+  for (var i = 0; i < document.form1.ads.length; i++) {
+
+    // ボタンが選択されている場合
+    if (document.form1.elements[i].checked) {
+
+      // textareaが空欄の場合は警告を出す
+      if ($("#text").val() == "") {
+        window.alert("Enter your opinion into the text area.");
+        return;
+      }
+
+      // 選択されているボタンを取得
+      a = document.form1.elements[i].value;
+
+      // Ajaxで送信するデータを作成
+      var result = {
+        id: $('#id').text(),
+        url: document.location.href,
+        date: new Date(),
+        selected: a,
+        impression: $("#text").val()
+      };
+
+      // 作成したデータをAjaxで送信
+      $.ajax({
+        url: dbpath,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(result),
+        dataType: 'xml'
+      }).done(function(data) { // success
+        window.location.href = next_page;
+      }).fail(function(data) { // error
+        window.alert(textStatus + ": Unable to connect to the server.");
+      });
+
+      break;
+
+      // ボタンが選択されていない場合
+    } else if (i == document.form1.ads.length - 1) {
+
+      window.alert('Please select the advertisement that appeared in the previous pages.');
+
+    }
+
+  }
+}
